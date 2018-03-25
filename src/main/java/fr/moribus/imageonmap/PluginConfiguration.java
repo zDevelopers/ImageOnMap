da26822aa49ddf881ab306e964c9de96e57ece60
kirbykirby56 <danny.wein@yahoo.com>
2018-03-24 21:44:06 -0400

    Add save-full-image and limit-x/y

--- src/main/java/fr/moribus/imageonmap/PluginConfiguration.java
+++ src/main/java/fr/moribus/imageonmap/PluginConfiguration.java
@@ -31,7 +31,10 @@ public final class PluginConfiguration extends Configuration
     static public ConfigurationItem<Locale> LANG = item("lang", Locale.class);
 
     static public ConfigurationItem<Boolean> COLLECT_DATA = item("collect-data", true);
+    static public ConfigurationItem<Boolean> SAVE_FULL_IMAGE = item("save-full-image", true);
 
     static public ConfigurationItem<Integer> MAP_GLOBAL_LIMIT = item("map-global-limit", 0, "Limit-map-by-server");
     static public ConfigurationItem<Integer> MAP_PLAYER_LIMIT = item("map-player-limit", 0, "Limit-map-by-player");
+    static public ConfigurationItem<Integer> LIMIT_SIZE_X = item("limit-map-size-x", 0);
+    static public ConfigurationItem<Integer> LIMIT_SIZE_Y = item("limit-map-size-y", 0);
 }
