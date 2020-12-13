d429be44bc3eb35b0b9c2c4c8cabdb5567e61623
Vlammar <valentin.jabre@gmail.com>
2020-12-13 15:57:03 +0100

    dead code and usage

--- src/main/java/fr/moribus/imageonmap/commands/maptool/GiveCommand.java
+++ src/main/java/fr/moribus/imageonmap/commands/maptool/GiveCommand.java
@@ -49,72 +49,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 
-@CommandInfo(name = "give", usageParameters = "<Player> <MapName> or <Player> <MapName> <Player where to find the map>")
+@CommandInfo(name = "give", usageParameters = "[playerFrom] <playername> <mapname>")
 public class GiveCommand extends IoMCommand {
-
-    //TODO passer avec une reconnaissance player/UUID, par défaut
-
-    /**
-     * Parse an argument given at a specific index, it will return a player depending on the given prefixe.
-     * Can be player:< username > or uuid:< uuid >
-     *
-     * @param index The index.
-     * @return The retrieved player.
-     * @throws CommandException If the value is invalid.
-     */
-    private OfflinePlayer parse(int index) throws CommandException {
-
-        String s = args[index].trim();
-        String[] subs = s.split(":");
-        //try {
-        //
-        if (subs.length == 1) {
-            return null;//temp
-            //return offlinePlayerParameter(index);
-        }
-
-        switch (subs[0]) {
-            case "player":
-                return null;//temp
-            // return offlinePlayerParameter(subs[1]);
-
-            case "uuid":
-                StringBuffer string = new StringBuffer(subs[1].toLowerCase());
-                //if there are no '-'
-                if (string.length() == 32) {
-                    //we try to fix it by adding - at pos 8,12,16,20
-                    Integer[] pos = {20, 16, 12, 8};
-                    for (int i : pos) {
-                        string = string.insert(i, "-");
-                    }
-                }
-
-                //if the given uuid is well formed with 8-4-4-4-12 = 36 chars in length (including '-')
-                if (string.length() == 36) {
-                    return Bukkit.getOfflinePlayer(UUID.fromString(string.toString()));
-                }
-
-                throwInvalidArgument(
-                        I.t("Invalid uuid, please provide an uuid of this form xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
-                                + " or xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"));
-                break;
-            case "bank":
-                throwInvalidArgument(I.t("Not supported yet"));
-                break;
-
-            default:
-                throwInvalidArgument(I.t("Invalid prefix, valid one are: player | uuid"));
-        }
-        /*} catch (InterruptedException | ExecutionException e) {
-            PluginLogger.warning(I.t("Can't access to mojang API to check the player UUID"));
-        }*/
-        return null;
-    }
 
     @Override
     protected void run() throws CommandException {
